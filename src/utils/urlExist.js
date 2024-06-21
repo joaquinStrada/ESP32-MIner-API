34a0c2fc19e60392a75240e34c76abd7392026ad
joaquinStrada <joaquinstrada@hotmail.com.ar>
2024-06-21 17:12:13 -0300

    Arreglado el url exists

--- src/utils/urlExist.js
+++ src/utils/urlExist.js
@@ -1,17 +1,11 @@
 import axios from 'axios'
-import https from 'https'
 import URL from 'url'
 
 const urlExist = url => new Promise(resolve => {
     const urlObject = URL.parse(url)
     const getURL = `https://${urlObject.hostname || urlObject.host || urlObject.href}`
     
-    axios.get(getURL, {
-        httpAgent: new https.Agent({
-            keepAlive: true,
-            timeout: 6000
-        })
-    })
+    axios.get(getURL)
         .then(res => {
             if (res.status && res.status >= 200 && res.status <= 299) {
                 resolve(true)
